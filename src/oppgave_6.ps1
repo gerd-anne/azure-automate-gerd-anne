ab68f210d13480a6160bb3281e077f7af2700119
Gerd Anne Buan <gerd_anne@outlook.com>
2021-11-26 15:18:02 +0100

    Utført oppgave 6

---
+++ src/oppgave_6.ps1
@@ -0,0 +1,67 @@
+
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = $kortstokk
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])$($kort.value)"
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+### Regn ut den samlede poengsummen til kortstokk
+#   Nummererte kort har poeng som angitt på kortet
+#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
+#   Ess (A) teller som 11 poeng
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J','Q','K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+$meg = $kortstokk[0..1]
+
+Write-Output "Meg: $(kortStokkTilStreng -kortstokk $meg)"
+
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+$magnus = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+Write-Output "Magnus: $(kortStokkTilStreng -kortstokk $magnus)"
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
