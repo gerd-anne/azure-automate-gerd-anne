9cc75e9156f3ef7cde68fd44418952c43fde6e4d
Gerd Anne Buan <gerd_anne@outlook.com>
2021-11-29 15:52:19 +0100

    Utført oppgave 10

---
+++ src/oppgave_10.ps1
@@ -0,0 +1,123 @@
+
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = $kortstokk
+    foreach ($kort in $kortstokk) {
+    $streng = $streng + "$($kort.suit[0])$($kort.value)"
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+### Regn ut den samlede poengsummen til kortstokk
+#   Nummererte kort har poeng som angitt på kortet
+#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
+#   Ess (A) teller som 11 poeng
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J','Q','K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+$meg = $kortstokk[0..1]
+
+#Write-Output "Meg: $(kortStokkTilStreng -kortstokk $meg)"
+
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+$magnus = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+#Write-Output "Magnus: $(kortStokkTilStreng -kortstokk $magnus)"
+
+#Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+function skrivUtResultat {
+    param (
+        [string]
+        $vinner,        
+        [object[]]
+        $kortStokkMagnus,
+        [object[]]
+        $kortStokkMeg        
+    )
+    Write-Output "Vinner: $vinner"
+    Write-Output "magnus | $(sumPoengKortstokk -kortstokk $magnus) | $(kortStokkTilStreng -kortstokk $magnus)"    
+    Write-Output "meg    | $(sumPoengKortstokk -kortstokk $meg) | $(kortStokkTilStreng -kortstokk $meg)"
+}
+
+# bruker 'blackjack' som et begrep - er 21
+$blackjack = 21
+
+#if ((sumPoengKortstokk -kortstokk $meg) -eq (sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+#    skrivUtResultat -vinner "draw" -kortStokkMagnus $magnus -kortStokkMeg $meg
+#    exit
+#}
+
+if ((sumPoengKortstokk -kortstokk $meg) -eq $blackjack) {
+    skrivUtResultat -vinner "meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack) {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+while ((sumPoengKortstokk -kortstokk $meg) -lt 17) {
+    $meg += $kortstokk[0]
+    $kortstokk = $kortstokk[1..$kortstokk.Count]
+}
+
+if ((sumPoengKortstokk -kortstokk $meg) -gt $blackjack) {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+while ((sumPoengKortstokk -kortstokk $magnus) -le (sumPoengKortstokk -kortstokk $meg)) {
+    $magnus += $kortstokk[0]
+    $kortstokk = $kortstokk[1..$kortstokk.Count]
+}
+
+if ((sumPoengKortstokk -kortstokk $magnus) -gt $blackjack) {
+    skrivUtResultat -vinner "meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+else {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+}
+exit
